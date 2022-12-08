007c7dbe132a695311a5cbf44288c90e7492bb7b
Ivan Havran <icrazy2001@gmail.com>
2022-12-08 14:01:52 +0200

    Added cities, now you can get JSON for all of them separately.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Функция получения json по api
-function getWeather() {
+function getWeather(a) {
     let array = {}; //тут у нас будет результат
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin', false);
+    xhr.open('GET', a, false);
     xhr.send();
     if (xhr.status != 200) {
         // вывести ошибку
@@ -12,9 +12,5 @@ function getWeather() {
         array = JSON.parse(xhr.responseText);
     }
     console.log(array);
+
 }
-//Добавляю координаты для каждого города
-let Kyiv = ;
-let Kharkiv = ;
-let Lviv = ;
-let Odessa = ;
